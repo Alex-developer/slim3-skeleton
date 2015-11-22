99b512ccf5478142efbfa660479675de08005e5a
Alex <alex.greenland@btinternet.com>
2015-11-22 16:15:12 +0000

    doctrine

--- app/settings.php
+++ app/settings.php
@@ -16,5 +16,22 @@ return [
             'name' => 'app',
             'path' => __DIR__ . '/../log/app.log',
         ],
+        'doctrine' => [
+            'meta' => [
+                'entity_path' => [
+                    'app/src/Entity'
+                ],
+                'auto_generate_proxies' => true,
+                'proxy_dir' =>  __DIR__.'/../cache/proxies',
+                'cache' => null,
+            ],
+            'connection' => [
+                'driver'   => 'pdo_mysql',
+                'host'     => 'localhost',
+                'dbname'   => 'alex_agsattrack',
+                'user'     => 'alex_agsattrack',
+                'password' => 'changeme',
+            ]
+        ],
     ],
 ];
